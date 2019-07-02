daa7b8a7c084ebd21b09ee20559304f057e647f2
Rodolfo Castillo <r.castillo@globant.com>
2019-07-02 14:41:31 -0500

    Initial Commit

---
+++ GalleryExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GalleryExample
+//
+//  Created by Rodolfo Castillo Vidrio on 7/2/19.
+//  Copyright © 2019 Rodolfo Castillo Vidrio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
